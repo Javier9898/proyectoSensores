c4ceb36357fb807b1d651f35067d107889c90b73
Javier Jiménez <javier.jimenez98@hotmail.com>
2020-12-08 10:35:38 -0500

    Sensor de huella para el Login hecho

---
+++ app/src/main/java/com/example/proyectosensores/Activity2.java
@@ -0,0 +1,32 @@
+package com.example.proyectosensores;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+    Button btnVolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        btnVolver =findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+
+    }
+
+}
--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -1,14 +1,102 @@
 package com.example.proyectosensores;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        final BiometricManager biometricManager = BiometricManager.from(this);
+
+        switch (biometricManager.canAuthenticate()){
+
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Toast.makeText(this,"Sensor de huella habilitado",Toast.LENGTH_LONG).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this,"No existe un sensor de huella en este dipositivo",Toast.LENGTH_LONG).show();
+                btnLogin.setVisibility(View.INVISIBLE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this,"Sensor Biometrico no disponible",Toast.LENGTH_LONG).show();
+                btnLogin.setVisibility(View.INVISIBLE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this,"Habilite el sensor de huella",Toast.LENGTH_LONG).show();
+                btnLogin.setVisibility(View.INVISIBLE);
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this,executor,new BiometricPrompt.AuthenticationCallback(){
+
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),"Inicio permitido",Toast.LENGTH_LONG).show();
+
+                //ESCRIBIR AQUI EL CAMBIO DE PANTALLAS
+                //openActivity2();
+                startActivity(new Intent(MainActivity.this, Activity2.class));
+
+
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        final BiometricPrompt.PromptInfo  promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Inicio")
+                .setDescription("Coloque su huella en el sensor")
+                .setNegativeButtonText("cancelar")
+                .build();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                biometricPrompt.authenticate(promptInfo);
+
+
+            }
+        });
+
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
